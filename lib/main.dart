f1d12c109be0ee90b76471befdfe76f40c9820af
Chris Jimenez <cgjimenez4@up.edu.ph>
2023-05-05 10:59:22 +0800

    add: add 'add accomms' page and 'admin dashboard'

---
+++ lib/main.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:stals_frontend/screens/signin.dart';
+import 'package:stals_frontend/screens/signup.dart';
+import 'package:stals_frontend/screens/homepage.dart';
+import 'package:stals_frontend/screens/homepage_signed.dart';
+import 'package:stals_frontend/screens/accomm.dart';
+import 'package:provider/provider.dart';
+import 'package:stals_frontend/providers/token_provider.dart';
+
+void main() {
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: ((context) => TokenProvider())),
+      ],
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CMSC 128 STALS',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const MyHomePage(title: 'CMSC 128'),
+        '/signin': (context) => const SignInPage(),
+        '/signup': (context) => const SignUpPage(),
+        '/homepage': (context) => const UnregisteredHomepage(),
+        '/signup_info': (context) => SignUpForm(),
+        '/signed_homepage': (context) => const RegisteredHomepage(),
+        '/accomm': (context) => const AccommPage()
+      },
+      theme: ThemeData(
+        fontFamily: 'SFProDisplayRegular',
+        //primarySwatch: MaterialColor(Color(0xff19535F), swatch),
+        //accentColor: Color(0xff0B7A75),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: Drawer(
+          child: ListView(padding: EdgeInsets.zero, children: [
+        ListTile(
+          title: const Text('Sign In Page'),
+          trailing: const Icon(Icons.person),
+          onTap: () {
+            Navigator.pushNamed(context, '/signin');
+          },
+        ),
+        ListTile(
+          title: const Text('Sign Up Page'),
+          trailing: const Icon(Icons.person_search_sharp),
+          onTap: () {
+            Navigator.pushNamed(context, '/signup');
+          },
+        ),
+        ListTile(
+          title: const Text('Unregistered Homepage'),
+          trailing: const Icon(Icons.note_sharp),
+          onTap: () {
+            Navigator.pushNamed(context, '/homepage');
+          },
+        ),
+        ListTile(
+          title: const Text('Registered Homepage'),
+          trailing: const Icon(Icons.home),
+          onTap: () {
+            Navigator.pushNamed(context, '/signed_homepage');
+          },
+        ),
+        ListTile(
+          title: const Text('Accommodation'),
+          trailing: const Icon(Icons.construction),
+          onTap: () {
+            Navigator.pushNamed(context, '/accomm');
+          },
+        ),
+        ListTile(
+          title: const Text('Logout'),
+          trailing: const Icon(Icons.logout),
+          onTap: () {
+            Provider.of<TokenProvider>(context, listen: false)
+                .removeToken("DO NOT REMOVE THIS PARAM");
+
+            print("Logged out");
+          },
+        ),
+      ])),
+      appBar: AppBar(
+        title: const Text("CMSC 128 STALS"),
+        backgroundColor: const Color.fromARGB(255, 67, 134, 221),
+      ),
+    );
+  }
+}
---
+++ lib/screens/homepage.dart
@@ -0,0 +1,161 @@
+import 'package:flutter/material.dart';
+import '../classes.dart';
+import '../UI_parameters.dart' as UIParameter;
+// COMPONENTS
+import '../components/accom_card.dart';
+import '../components/search_bar.dart' as sb;
+
+class UnregisteredHomepage extends StatefulWidget {
+  const UnregisteredHomepage({Key? key}) : super(key: key);
+
+  @override
+  _UnregisteredHomepageState createState() => _UnregisteredHomepageState();
+}
+
+class _UnregisteredHomepageState extends State<UnregisteredHomepage> {
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  Widget build(BuildContext context) {
+    /*
+    DUMMY OBJECT
+    <Object will come from database fetch later>
+    */
+    var accom = AccomCardDetails("jk23fvgw23", "Centrro Residences",
+        "Description of Centrro Residences", "assets/images/room_stock.jpg", 3);
+
+    return Scaffold(
+        key: scaffoldKey,
+        appBar: AppBar(
+            backgroundColor: Color(0xffF0F3F5),
+            elevation: 0,
+            // hamburger icon for profile
+            // opens left drawer on tap
+            leading: IconButton(
+              icon: Icon(Icons.menu),
+              color: Color(0xff0B7A75),
+              onPressed: () {
+                if (scaffoldKey.currentState!.isDrawerOpen) {
+                  //scaffoldKey.currentState!.closeDrawer();
+                  //close drawer, if drawer is open
+                } else {
+                  scaffoldKey.currentState!.openDrawer();
+                  //open drawer, if drawer is closed
+                }
+              },
+            ),
+            // search bar at the top of the homepage
+            title: sb.SearchBar(
+              hintText: 'Search',
+              onChanged: (value) {
+                /* PUT SEARCH FUNCTION HERE */
+              },
+            ),
+            // filter icon for filtered search
+            // opens right drawer on tap
+            // thinking to implement yung katulad ng filter sa shoppee?
+            actions: <Widget>[
+              Builder(
+                builder: (context) {
+                  return IconButton(
+                    icon: Icon(Icons.filter_alt),
+                    color: Color(0xff7B2D26),
+                    onPressed: () {
+                      Scaffold.of(context).openEndDrawer();
+                    },
+                  );
+                },
+              )
+            ]),
+        // the left drawer
+        drawer: Drawer(
+          // Add a ListView to the drawer. This ensures the user can scroll
+          // through the options in the drawer if there isn't enough vertical
+          // space to fit everything.
+          child: ListView(
+            // Important: Remove any padding from the ListView.
+            padding: EdgeInsets.zero,
+            children: [
+              const SizedBox(
+                height: 100,
+                child: DrawerHeader(
+                  decoration: BoxDecoration(
+                    color: Color(0xff0B7A75),
+                  ),
+                  child: Text('PROFILE'),
+                ),
+              ),
+              ListTile(
+                title: const Text('Item 1'),
+                onTap: () {
+                  // Update the state of the app.
+                  // ...
+                },
+              ),
+              ListTile(
+                title: const Text('Item 2'),
+                onTap: () {
+                  // Update the state of the app.
+                  // ...
+                },
+              ),
+            ],
+          ),
+        ),
+        // the right drawer
+        endDrawer: Drawer(
+          // Add a ListView to the drawer. This ensures the user can scroll
+          // through the options in the drawer if there isn't enough vertical
+          // space to fit everything.
+          child: ListView(
+            // Important: Remove any padding from the ListView.
+            padding: EdgeInsets.zero,
+            children: [
+              const SizedBox(
+                height: 100,
+                child: DrawerHeader(
+                  decoration: BoxDecoration(
+                    color: Color(0xff7B2D26),
+                  ),
+                  child: Text('FILTER'),
+                ),
+              ),
+              ListTile(
+                title: const Text('Item 1'),
+                onTap: () {
+                  // Update the state of the app.
+                  // ...
+                },
+              ),
+              ListTile(
+                title: const Text('Item 2'),
+                onTap: () {
+                  // Update the state of the app.
+                  // ...
+                },
+              ),
+            ],
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: Container(
+            // get the height and width of the device
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            padding: EdgeInsets.all(20),
+            color: Color(0xffF0F3F5),
+            child: Center(
+              child: Column(
+                children: [
+                  // 1 accomm card for demo
+                  // to create a component later that will build all the AccomCard of all fetched accommodation from database
+                  AccomCard(
+                    details: accom,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ));
+  }
+}
