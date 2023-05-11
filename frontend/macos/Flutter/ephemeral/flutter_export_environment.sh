833ea385c9a97237b273dfef1fcb1eeec58c6ed9
SadgeZy <sdtamargo@up.edu.ph>
2023-05-11 14:04:22 +0800

    gradle updates

---
+++ frontend/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=S:\flutter"
+export "FLUTTER_APPLICATION_PATH=S:\School\CMSC 128\stals_frontend"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
