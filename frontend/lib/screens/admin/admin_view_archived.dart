03b1df991228dd52ccef0ee4c734e3f89fcd67dd
rtpaguila <rtpaguila@up.edu.ph>
2023-05-25 12:47:26 +0800

    add: backend integration
    will now fetch data from backend and database and display it on frontend

--- frontend/lib/screens/admin/admin_view_archived.dart
+++ frontend/lib/screens/admin/admin_view_archived.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import '../../classes.dart';
 import '../../UI_parameters.dart' as UIParameter;
 import '../../components/accom_card.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class ViewArchivedAccommodations extends StatefulWidget {
   const ViewArchivedAccommodations({super.key});
@@ -13,82 +15,125 @@ class ViewArchivedAccommodations extends StatefulWidget {
 
 class _ViewArchivedAccommodationsState
     extends State<ViewArchivedAccommodations> {
+      Future<List<AccomCardDetails>>? _accommodationsFuture;
   final scaffoldKey = GlobalKey<ScaffoldState>();
   // int _selectedIndex = 2;
 
   @override
+  void initState() {
+    super.initState();
+    _accommodationsFuture = fetchAccommodations();
+  }
+
+  Future<List<AccomCardDetails>> fetchAccommodations() async {
+  final response = await http.get(Uri.parse('http://127.0.0.1:8000/view-all-establishment/'));
+
+  if (response.statusCode == 200) {
+    List jsonResponse = jsonDecode(response.body);
+    return jsonResponse.map((accommodation) => AccomCardDetails.fromJson(accommodation)).toList();
+  } else {
+    throw Exception('Failed to load accommodations');
+  }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    var accom1 = AccomCardDetails(
-        "accommId1",
-        "Accommodation1",
-        "This is a description for accommodation 1",
-        "assets/images/room_stock.jpg",
-        3,
-        true,
-        true);
-    var accom2 = AccomCardDetails(
-        "accommId2",
-        "Accommodation2",
-        "This is a description for accommodation 2",
-        "assets/images/room_stock.jpg",
-        5,
-        true,
-        true);
-    var accom3 = AccomCardDetails(
-        "accommId3",
-        "Accommodation3",
-        "This is a description for accommodation 3",
-        "assets/images/room_stock.jpg",
-        2,
-        true,
-        true);
 
-    var archivedAccomms = Column(children: [
-      const Padding(
-        padding: EdgeInsets.symmetric(vertical: 5),
-      ),
-      const Padding(
-        padding: EdgeInsets.only(left: 10),
-        child: Align(
-          alignment: Alignment.topLeft,
-          child: Text("Archived", style: TextStyle(fontSize: 18)),
-        ),
-      ),
-      const Padding(
-        padding: EdgeInsets.symmetric(vertical: 7),
-      ),
-      AccomCard(details: accom1),
-      const Padding(
-        padding: EdgeInsets.symmetric(vertical: 7),
-      ),
-      AccomCard(details: accom2),
-      const Padding(
-        padding: EdgeInsets.symmetric(vertical: 7),
-      ),
-      AccomCard(details: accom3),
-    ]);
 
+    // var accom1 = AccomCardDetails(
+    //     "accommId1",
+    //     "Accommodation1",
+    //     "This is a description for accommodation 1",
+    //     "assets/images/room_stock.jpg",
+    //     3,
+    //     true,
+    //     true);
+    // var accom2 = AccomCardDetails(
+    //     "accommId2",
+    //     "Accommodation2",
+    //     "This is a description for accommodation 2",
+    //     "assets/images/room_stock.jpg",
+    //     5,
+    //     true,
+    //     true);
+    // var accom3 = AccomCardDetails(
+    //     "accommId3",
+    //     "Accommodation3",
+    //     "This is a description for accommodation 3",
+    //     "assets/images/room_stock.jpg",
+    //     2,
+    //     true,
+    //     true);
+
+    // var archivedAccomms = Column(children: [
+    //   const Padding(
+    //     padding: EdgeInsets.symmetric(vertical: 5),
+    //   ),
+    //   const Padding(
+    //     padding: EdgeInsets.only(left: 10),
+    //     child: Align(
+    //       alignment: Alignment.topLeft,
+    //       child: Text("Archived", style: TextStyle(fontSize: 18)),
+    //     ),
+    //   ),
+    //   const Padding(
+    //     padding: EdgeInsets.symmetric(vertical: 7),
+    //   ),
+    //   AccomCard(details: accom1),
+    //   const Padding(
+    //     padding: EdgeInsets.symmetric(vertical: 7),
+    //   ),
+    //   AccomCard(details: accom2),
+    //   const Padding(
+    //     padding: EdgeInsets.symmetric(vertical: 7),
+    //   ),
+    //   AccomCard(details: accom3),
+    // ]);
+
+    // return Scaffold(
+    //   body: SingleChildScrollView(
+    //     child: Column(children: [
+    //       Container(
+    //         height: MediaQuery.of(context).size.height,
+    //         width: MediaQuery.of(context).size.width,
+    //         padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+    //         color: UIParameter.WHITE,
+    //         child: SingleChildScrollView(
+    //           child: Column(children: [
+    //             const Padding(
+    //               padding: EdgeInsets.symmetric(vertical: 10),
+    //             ),
+    //             archivedAccomms,
+    //             const Padding(
+    //               padding: EdgeInsets.symmetric(vertical: 30),
+    //             ),
+    //           ]),
+    //         ),
+    //       ),
+    //     ]),
+    //   ),
+    // );
     return Scaffold(
       body: SingleChildScrollView(
-        child: Column(children: [
-          Container(
-            height: MediaQuery.of(context).size.height,
-            width: MediaQuery.of(context).size.width,
-            padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
-            color: UIParameter.WHITE,
-            child: SingleChildScrollView(
-              child: Column(children: [
-                const Padding(
-                  padding: EdgeInsets.symmetric(vertical: 10),
-                ),
-                archivedAccomms,
-                const Padding(
-                  padding: EdgeInsets.symmetric(vertical: 30),
-                ),
-              ]),
-            ),
-          ),
-        ]),
+        child: FutureBuilder<List<AccomCardDetails>>(
+          future: _accommodationsFuture,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              List<AccomCardDetails> accommodations = snapshot.data!;
+              return Column(
+                children: accommodations.map((accommodation) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 7),
+                    child: AccomCard(details: accommodation),
+                  );
+                }).toList(),
+              );
+            } else if (snapshot.hasError) {
+              return Text('Error: ${snapshot.error}');
+            }
+            return CircularProgressIndicator();
+          },
+        ),
       ),
     );
   }
