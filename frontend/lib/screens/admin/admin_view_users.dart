13a749abc0de6abf660a48ff1ae4e754e74c6a6f
hansehansehanse <hahernandez@up.edu.ph>
2023-05-25 18:57:14 +0800

    feat: admin view users

--- frontend/lib/screens/admin/admin_view_users.dart
+++ frontend/lib/screens/admin/admin_view_users.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+
+import '../../models/user_model.dart';
 
 class ViewUsersPage extends StatefulWidget {
   const ViewUsersPage({super.key});
@@ -6,94 +9,133 @@ class ViewUsersPage extends StatefulWidget {
   State<ViewUsersPage> createState() => _ViewUsersPageState();
 }
 
+
 class _ViewUsersPageState extends State<ViewUsersPage> {
-  List<dynamic> users = [
-    "John Smith",
-    "Juan de la Cruz",
-    "Manuel Quezon III",
-    "Jose Rizal",
-    "Roland Dagon"
-  ];
+
+  List<User> users = [];
+  bool isLoading = false;
+  String apiUrl = 'http://127.0.0.1:8000/view-all-users/';
+
+  @override
+  void initState() {
+    super.initState();
+    fetchUsers();
+  }
+
+  Future<void> fetchUsers() async {
+    setState(() {
+      isLoading = true;
+    });
+
+    try {
+      Response response = await Dio().get(apiUrl);
+      print(response.data);
+
+      if (response.statusCode == 200) {
+        List<dynamic> data = response.data;
+        print(data);
+
+        List<User> fetchedUsers =
+            data.map((user) => User.fromJson(user)).toList();
+        setState(() {
+          users = fetchedUsers;
+        });
+      }
+    } catch (error) {
+      print(error.toString());
+    }
+
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  
 
   int selectedIndex = 0;
 
+  // @override
+  // Widget build(BuildContext context) {
+    
+  //   return Scaffold(
+  //     backgroundColor: Colors.white,
+  //     body: Container(
+  //       width: 460,
+  //       decoration: BoxDecoration(
+  //         color: Colors.white,
+  //         border: Border.all(),
+  //         borderRadius: BorderRadius.circular(18),
+  //         boxShadow: const [
+  //           BoxShadow(
+  //             color: Colors.black12,
+  //             blurRadius: 3.0,
+  //             offset: Offset(0, -5.0),
+  //           ),
+  //         ],
+  //       ),
+  //       child: ListView.builder(
+  //         itemCount: users.length,
+  //         itemBuilder: ((context, index) {
+  //           // var user = users[index];
+  //           return ListTile(
+  //             onTap: () {},
+  //             leading: Icon(Icons.person),
+  //             title: Text("${users[index]}"),
+  //             trailing: ElevatedButton(
+  //               style: ElevatedButton.styleFrom(
+  //                   shape: const StadiumBorder(),
+  //                   backgroundColor: Color.fromRGBO(212, 57, 65, 1),
+  //                   padding: const EdgeInsets.symmetric(
+  //                       horizontal: 26, vertical: 13),
+  //                   textStyle: const TextStyle(fontSize: 12)),
+  //               onPressed: () {},
+  //               child: null,
+  //             ),
+  //           );
+  //         }),
+  //       ),
+  //     ),
+
+  //   );
+  // }
+
   @override
   Widget build(BuildContext context) {
-    // final profileIcon = SizedBox(
-    //   height: 100,
-    //   child: Row(mainAxisAlignment: MainAxisAlignment.center, children: const [
-    //     Image(image: AssetImage('icons/profile.png')),
-    //     Padding(padding: EdgeInsets.symmetric(horizontal: 10))
-    //   ]),
-    // );
-
-    // final backButton = Center(
-    //     child: SizedBox(
-    //         width: 100,
-    //         child: Padding(
-    //           padding: const EdgeInsets.only(top: 50),
-    //           child: ElevatedButton(
-    //             style: ElevatedButton.styleFrom(
-    //                 shape: const StadiumBorder(),
-    //                 backgroundColor: const Color.fromARGB(255, 67, 134, 221),
-    //                 padding: const EdgeInsets.symmetric(
-    //                     horizontal: 26, vertical: 13),
-    //                 textStyle: const TextStyle(fontSize: 12)),
-    //             onPressed: () async {
-    //               Navigator.pop(context);
-    //             },
-    //             child:
-    //                 const Text('Back', style: TextStyle(color: Colors.white)),
-    //           ),
-    //         )));
-
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Container(
-        width: 460,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          border: Border.all(),
-          borderRadius: BorderRadius.circular(18),
-          boxShadow: const [
-            BoxShadow(
-              color: Colors.black12,
-              blurRadius: 3.0,
-              offset: Offset(0, -5.0),
-            ),
-          ],
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Users List'),
         ),
-        child: ListView.builder(
-          itemCount: users.length,
-          itemBuilder: ((context, index) {
-            // var user = users[index];
-            return ListTile(
-              onTap: () {},
-              leading: Icon(Icons.person),
-              title: Text("${users[index]}"),
-              trailing: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                    shape: const StadiumBorder(),
-                    backgroundColor: Color.fromRGBO(212, 57, 65, 1),
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 26, vertical: 13),
-                    textStyle: const TextStyle(fontSize: 12)),
-                onPressed: () {},
-                child: null,
+        body: isLoading
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : ListView.builder(
+                itemCount: users.length,
+                itemBuilder: (context, index) {
+
+
+                  return ListTile(
+                    leading: Icon(Icons.person),
+                    title: Text(
+                      '${users[index].firstName} ${users[index].lastName}\n'
+                      '${users[index].userType}',
+                    ),
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Username: ${users[index].username}'),
+                        Text('Email: ${users[index].email}'),
+                        Text('Verified: ${users[index].verified}'),
+                        Text('Archived: ${users[index].archived}'),
+                      ],
+                    ),
+                  );
+
+                },
               ),
-            );
-          }),
-        ),
       ),
-      // bottomNavigationBar: NavigationBar(
-      //   selectedIndex: selectedIndex,
-      //   onDestinationSelected: (int index) {
-      //     setState(() {
-      //       selectedIndex = index;
-      //     });
-      //   },
-      //   destinations: [],
-      // ),
     );
   }
+
 }
