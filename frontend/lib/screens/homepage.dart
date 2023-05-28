9f6976a24e16bd41a8cb987f4a8791d7327a5f33
Chris Jimenez <cgjimenez4@up.edu.ph>
2023-05-28 13:33:50 +0800

    feat: removed the temporary frontend page, check comments in main.dart for more info

--- frontend/lib/screens/homepage.dart
+++ frontend/lib/screens/homepage.dart
@@ -12,18 +12,12 @@ import 'dart:convert';
 import 'dart:async';
 import 'dart:io';
 
-
 class UnregisteredHomepage extends StatefulWidget {
   const UnregisteredHomepage({Key? key}) : super(key: key);
 
-
   @override
   _UnregisteredHomepageState createState() => _UnregisteredHomepageState();
-  
-  
 }
-
-
 
 class _UnregisteredHomepageState extends State<UnregisteredHomepage> {
   final scaffoldKey = GlobalKey<ScaffoldState>();
@@ -35,9 +29,9 @@ class _UnregisteredHomepageState extends State<UnregisteredHomepage> {
     _accommodationsFuture = fetchAllAccommodations();
   }
 
-
   Future<List<AccomCardDetails>> fetchAllAccommodations() async {
-    final response = await http.get(Uri.parse('http://127.0.0.1:8000/view-all-establishment/'));
+    final response = await http
+        .get(Uri.parse('http://127.0.0.1:8000/view-all-establishment/'));
 
     if (response.statusCode == 200) {
       List jsonResponse = jsonDecode(response.body);
@@ -45,7 +39,7 @@ class _UnregisteredHomepageState extends State<UnregisteredHomepage> {
           .map((accommodation) => AccomCardDetails.fromJson(accommodation))
           .toList();
 
-       // Apply the filter
+      // Apply the filter
       return accommodations;
     } else {
       throw Exception('Failed to load accommodations');
@@ -63,80 +57,80 @@ class _UnregisteredHomepageState extends State<UnregisteredHomepage> {
     //     "Description of Centrro Residences", "assets/images/room_stock.jpg", 3,false,false);
 
     return Scaffold(
-        key: scaffoldKey,
-        appBar: AppBar(
-            backgroundColor: UIParameter.WHITE,
-            elevation: 0,
-            // hamburger icon for profile
-            // opens left drawer on tap
-            leading: IconButton(
-              icon: const Icon(Icons.menu),
-              color: UIParameter.LIGHT_TEAL,
-              onPressed: () {
-                if (scaffoldKey.currentState!.isDrawerOpen) {
-                  //scaffoldKey.currentState!.closeDrawer();
-                  //close drawer, if drawer is open
-                } else {
-                  scaffoldKey.currentState!.openDrawer();
-                  //open drawer, if drawer is closed
-                }
-              },
-            ),
-            // search bar at the top of the homepage
-            title: CustomSearchBar(
-              hintText: 'Search',
-              onChanged: (value) {
-                /* PUT SEARCH FUNCTION HERE */
-              },
-            ),
-            // filter icon for filtered search
-            actions: <Widget>[
-              Builder(
-                builder: (context) {
-                  return IconButton(
-                    icon: const Icon(Icons.filter_alt),
-                    color: UIParameter.MAROON,
-                    onPressed: () {
-                      // cannot use filter if not signed-in
-                    },
-                  );
-                },
-              )
-            ]),
-        // the left drawer
-        drawer: Drawer(
-          // Add a ListView to the drawer. This ensures the user can scroll
-          // through the options in the drawer if there isn't enough vertical
-          // space to fit everything.
-          child: ListView(
-            // Important: Remove any padding from the ListView.
-            padding: EdgeInsets.zero,
-            children: [
-              SizedBox(
-                height: 100,
-                child: DrawerHeader(
-                  decoration: BoxDecoration(
-                    color: UIParameter.LIGHT_TEAL,
-                  ),
-                  child: const Text(''),
-                ),
-              ),
-              ListTile(
-                title: const Text('Sign In'),
-                onTap: () {
-                  Navigator.pushNamed(context, '/signin');
-                },
-              ),
-              ListTile(
-                title: const Text('Sign Up'),
-                onTap: () {
-                  Navigator.pushNamed(context, '/signup');
-                },
-              ),
-            ],
+      key: scaffoldKey,
+      appBar: AppBar(
+          backgroundColor: UIParameter.WHITE,
+          elevation: 0,
+          // hamburger icon for profile
+          // opens left drawer on tap
+          leading: IconButton(
+            icon: const Icon(Icons.menu),
+            color: UIParameter.LIGHT_TEAL,
+            onPressed: () {
+              if (scaffoldKey.currentState!.isDrawerOpen) {
+                //scaffoldKey.currentState!.closeDrawer();
+                //close drawer, if drawer is open
+              } else {
+                scaffoldKey.currentState!.openDrawer();
+                //open drawer, if drawer is closed
+              }
+            },
           ),
+          // search bar at the top of the homepage
+          title: CustomSearchBar(
+            hintText: 'Search',
+            onChanged: (value) {
+              /* PUT SEARCH FUNCTION HERE */
+            },
+          ),
+          // filter icon for filtered search
+          actions: <Widget>[
+            Builder(
+              builder: (context) {
+                return IconButton(
+                  icon: const Icon(Icons.filter_alt),
+                  color: UIParameter.MAROON,
+                  onPressed: () {
+                    // cannot use filter if not signed-in
+                  },
+                );
+              },
+            )
+          ]),
+      // the left drawer
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            SizedBox(
+              height: 100,
+              child: DrawerHeader(
+                decoration: BoxDecoration(
+                  color: UIParameter.LIGHT_TEAL,
+                ),
+                child: const Text(''),
+              ),
+            ),
+            ListTile(
+              title: const Text('Sign In'),
+              onTap: () {
+                Navigator.pushNamed(context, '/signin');
+              },
+            ),
+            ListTile(
+              title: const Text('Sign Up'),
+              onTap: () {
+                Navigator.pushNamed(context, '/signup');
+              },
+            ),
+          ],
         ),
-       body: SingleChildScrollView(
+      ),
+      body: SingleChildScrollView(
         child: FutureBuilder<List<AccomCardDetails>>(
           future: _accommodationsFuture,
           builder: (context, snapshot) {
