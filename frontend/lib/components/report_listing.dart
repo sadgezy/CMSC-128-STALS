ed7575d98872160c963bb74a7dfa1926ac8b9a02
cherrycontaoi <cbcontaoi@up.edu.ph>
2023-05-30 10:57:22 +0800

    feat: Added pop up for reporting a listing

---
+++ frontend/lib/components/report_listing.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:stals_frontend/UI_parameters.dart' as UIParams;
+
+class ReportListing extends StatefulWidget {
+  const ReportListing({super.key});
+
+  @override
+  State<ReportListing> createState() => _ReportListingState();
+}
+
+class _ReportListingState extends State<ReportListing> {
+  int value = 0;
+  Widget CustomRadioButton(String text, int index) {
+    return Padding(
+        padding: EdgeInsets.all(5),
+        child: SizedBox(
+          height: 50,
+          width: 200,
+          child: OutlinedButton(
+            onPressed: () {
+              setState(() {
+                value = index;
+              });
+            },
+            child: Text(
+              text,
+              style: TextStyle(
+                color: (value == index) ? UIParams.MAROON : Colors.black,
+              ),
+            ),
+            style: OutlinedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)),
+              side: BorderSide(
+                  color: (value == index) ? UIParams.MAROON : Colors.black,
+                  width: 2),
+            ),
+          ),
+        ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CustomRadioButton("Inactive Owner", 1),
+        CustomRadioButton("Inaccurate Details", 2),
+        CustomRadioButton("Fraudulent Listing", 3),
+        CustomRadioButton("Offensive Content", 4),
+      ],
+    );
+  }
+}
