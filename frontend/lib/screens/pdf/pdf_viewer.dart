e178da7a791101fd3399f8c7e2013e0ce1c02009
CesiumNest <efberico@up.edu.ph>
2023-06-02 04:37:45 +0800

    updated contents for pdf

--- frontend/lib/screens/pdf/pdf_viewer.dart
+++ frontend/lib/screens/pdf/pdf_viewer.dart
@@ -12,25 +12,27 @@ import 'pdf_model.dart';
 //DUMMY FOR PDF
 List<PDFData> estabData = [
   PDFData(
-      "PDF 1",
+      "Bahay ni Kuya",
       "assets/images/room_stock.jpg",
       "Within Campus",
-      "11 L Street",
+      "11 L Street Los Banos City",
       "Dormitory",
       "Ceat Students.",
-      "0000000",
+      "09172222222",
       "gg@wp.com",
-      "mabango", []),
+      "mabango, friendly, malinis",
+      [Room(5000, 1, false), Room(3000, 1, true), Room(10000, 4, true)]),
   PDFData(
       "PDF 2",
       "assets/images/room_stock.jpg",
       "Beyond Junction",
-      "B7 L23 Jade St.",
+      "B7 L23 Jade St. Calamba City",
       "House",
       "Working",
-      "0000000",
+      "09172222222",
       "gg@wp.com",
-      "toilet", []),
+      "toilet, aircon, pets allowed",
+      [Room(22000, 8, true)]),
 ];
 
 class PDFViewScreen extends StatelessWidget {
@@ -62,39 +64,106 @@ class PDFViewScreen extends StatelessWidget {
       pw.Page(
         pageFormat: format,
         build: (pw.Context context) {
-          return buildEstabTiles(estabData);
+          return pw.Column(
+            children: [
+              for (var estab in estabData)
+                pw.Container(
+                    decoration: pw.BoxDecoration(border: pw.Border.all()),
+                    child: pw.Column(children: [
+                      pw.Row(
+                        children: [
+                          // pw.Expanded(
+                          // // child: pw.Icon(Icons.warning)
+                          // child: pw.Image(
+                          //   pw.MemoryImage(await _loadImage(estab.image)),
+                          // ),
+                          // ),
+                          pw.Expanded(
+                            child: pw.Column(
+                              crossAxisAlignment: pw.CrossAxisAlignment.start,
+                              mainAxisAlignment: pw.MainAxisAlignment.start,
+                              children: [
+                                pw.Text(estab.estabName,
+                                    style: pw.Theme.of(context)
+                                        .defaultTextStyle
+                                        .copyWith(
+                                            fontWeight: pw.FontWeight.bold)),
+                                pw.Text(estab.approxLoc),
+                                // pw.Text(estab.exactLoc),
+                                pw.Text(estab.estabType),
+                                pw.Text(estab.tenantType),
+                                pw.Text(estab.utilities),
+                                pw.SizedBox(height: 10),
+                                pw.Text("Owner Information",
+                                    style: pw.Theme.of(context)
+                                        .defaultTextStyle
+                                        .copyWith(
+                                            fontWeight: pw.FontWeight.bold)),
+                                pw.Text(estab.ownerContact),
+                                pw.Text(estab.ownerEmail),
+                              ],
+                            ),
+                          ),
+                          pw.Expanded(
+                              child: pw.Column(
+                            crossAxisAlignment: pw.CrossAxisAlignment.start,
+                            mainAxisAlignment: pw.MainAxisAlignment.start,
+                            children: [
+                              for (var r in estab.rooms)
+                                pw.Row(
+                                  children: [
+                                    // pw.Icon(Icons.check_circle_outline, ) // TO IMPLEMENT: ICONS CHECK or X for availability
+                                    pw.Text(
+                                        "PHP${r.price.toString()} with capacity of ${r.capacity.toString()}"),
+                                    if (r.available)
+                                      pw.Text(" [AVAILABLE]")
+                                    else
+                                      pw.Text(" [UNAVAILABLE]")
+                                  ],
+                                )
+                            ],
+                          ))
+                        ],
+                      ),
+                      pw.SizedBox(height: 20),
+                    ]))
+            ],
+          );
         },
       ),
     );
     return pdf.save();
   }
 
-  pw.Widget buildEstabTiles(List<PDFData> estabData) => pw.Container(
-        child: pw.Column(
-          children: [
-            for (var estab in estabData)
-              pw.Row(
-                children: [
-                  // pw.Expanded(
-                  // child: pw.Icon(Icons.warning)
-                  // child: pw.Image(
-                  //   pw.MemoryImage(await _loadImage(estab.image)),
-                  // ),
-                  // ),
-                  pw.Expanded(
-                    child: pw.Column(
-                      children: [
-                        pw.Text(estab.estabName),
-                        pw.Text(estab.approxLoc),
-                        pw.Text(estab.exactLoc),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-          ],
-        ),
-      );
+  // pw.Widget buildEstabTiles(List<PDFData> estabData) => pw.Container(
+  //       child: pw.Column(
+  //         children: [
+  //           for (var estab in estabData)
+  //             pw.Row(
+  //               children: [
+  //                 // pw.Expanded(
+  //                 // child: pw.Icon(Icons.warning)
+  //                 // child: pw.Image(
+  //                 //   pw.MemoryImage(await _loadImage(estab.image)),
+  //                 // ),
+  //                 // ),
+  //                 pw.Expanded(
+  //                   child: pw.Column(
+  //                     children: [
+  //                       pw.Text(estab.estabName,
+  //                           style: pw.Theme.of(context)
+  //                               .defaultTextStyle
+  //                               .copyWith(fontWeight: pw.FontWeight.bold)),
+  //                       pw.Text(estab.approxLoc),
+  //                       pw.Text(estab.exactLoc),
+  //                     ],
+  //                   ),
+  //                 ),
+  //               ],
+  //             ),
+  //         ],
+  //       ),
+  //     );
 
   Future<Uint8List> _loadImage(String imagePath) async {
     final ByteData imageData = await rootBundle.load(imagePath);
