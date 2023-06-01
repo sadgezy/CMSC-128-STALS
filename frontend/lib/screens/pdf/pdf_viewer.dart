cf4b2473c9fa7388dcc41036a88ee44cf454faec
CesiumNest <efberico@up.edu.ph>
2023-06-02 05:40:09 +0800

    added multi page functionality for the pdf

--- frontend/lib/screens/pdf/pdf_viewer.dart
+++ frontend/lib/screens/pdf/pdf_viewer.dart
@@ -33,6 +33,94 @@ List<PDFData> estabData = [
       "gg@wp.com",
       "toilet, aircon, pets allowed",
       [Room(22000, 8, true)]),
+  PDFData(
+      "Bahay ni Kuya",
+      "assets/images/room_stock.jpg",
+      "Within Campus",
+      "11 L Street Los Banos City",
+      "Dormitory",
+      "Ceat Students.",
+      "09172222222",
+      "gg@wp.com",
+      "mabango, friendly, malinis",
+      [Room(5000, 1, false), Room(3000, 1, true), Room(10000, 4, true)]),
+  PDFData(
+      "PDF 2",
+      "assets/images/room_stock.jpg",
+      "Beyond Junction",
+      "B7 L23 Jade St. Calamba City",
+      "House",
+      "Working",
+      "09172222222",
+      "gg@wp.com",
+      "toilet, aircon, pets allowed",
+      [Room(22000, 8, true)]),
+  PDFData(
+      "Bahay ni Kuya",
+      "assets/images/room_stock.jpg",
+      "Within Campus",
+      "11 L Street Los Banos City",
+      "Dormitory",
+      "Ceat Students.",
+      "09172222222",
+      "gg@wp.com",
+      "mabango, friendly, malinis",
+      [Room(5000, 1, false), Room(3000, 1, true), Room(10000, 4, true)]),
+  PDFData(
+      "PDF 2",
+      "assets/images/room_stock.jpg",
+      "Beyond Junction",
+      "B7 L23 Jade St. Calamba City",
+      "House",
+      "Working",
+      "09172222222",
+      "gg@wp.com",
+      "toilet, aircon, pets allowed",
+      [Room(22000, 8, true)]),
+  PDFData(
+      "Bahay ni Kuya",
+      "assets/images/room_stock.jpg",
+      "Within Campus",
+      "11 L Street Los Banos City",
+      "Dormitory",
+      "Ceat Students.",
+      "09172222222",
+      "gg@wp.com",
+      "mabango, friendly, malinis",
+      [Room(5000, 1, false), Room(3000, 1, true), Room(10000, 4, true)]),
+  PDFData(
+      "PDF 2",
+      "assets/images/room_stock.jpg",
+      "Beyond Junction",
+      "B7 L23 Jade St. Calamba City",
+      "House",
+      "Working",
+      "09172222222",
+      "gg@wp.com",
+      "toilet, aircon, pets allowed",
+      [Room(22000, 8, true)]),
+  PDFData(
+      "Bahay ni Kuya",
+      "assets/images/room_stock.jpg",
+      "Within Campus",
+      "11 L Street Los Banos City",
+      "Dormitory",
+      "Ceat Students.",
+      "09172222222",
+      "gg@wp.com",
+      "mabango, friendly, malinis",
+      [Room(5000, 1, false), Room(3000, 1, true), Room(10000, 4, true)]),
+  PDFData(
+      "PDF 2",
+      "assets/images/room_stock.jpg",
+      "Beyond Junction",
+      "B7 L23 Jade St. Calamba City",
+      "House",
+      "Working",
+      "09172222222",
+      "gg@wp.com",
+      "toilet, aircon, pets allowed",
+      [Room(22000, 8, true)]),
 ];
 
 class PDFViewScreen extends StatelessWidget {
@@ -60,85 +148,89 @@ class PDFViewScreen extends StatelessWidget {
     final pdf = pw.Document(version: PdfVersion.pdf_1_5, compress: false);
     // final font = await PdfGoogleFonts.nunitoExtraLight();
 
-    pdf.addPage(
-      pw.Page(
-        pageFormat: format,
-        build: (pw.Context context) {
-          return pw.Column(
-            children: [
-              for (var estab in estabData)
-                pw.Container(
-                    decoration: pw.BoxDecoration(border: pw.Border.all()),
-                    // padding: const pw.EdgeInsets.all(16.0),
-                    child: pw.Column(children: [
-                      pw.SizedBox(
-                        height: 10,
-                      ),
-                      pw.Row(
-                        children: [
-                          pw.SizedBox(width: 10),
-                          // IMAGE INSERT HERE
-                          pw.Expanded(
-                            child: pw.Column(
-                              crossAxisAlignment: pw.CrossAxisAlignment.start,
-                              mainAxisAlignment: pw.MainAxisAlignment.start,
-                              children: [
-                                pw.Text(estab.estabName,
-                                    style: pw.Theme.of(context)
-                                        .defaultTextStyle
-                                        .copyWith(
-                                            fontWeight: pw.FontWeight.bold)),
-                                pw.Text(estab.approxLoc),
-                                // pw.Text(estab.exactLoc),
-                                pw.Text(estab.estabType),
-                                pw.Text(estab.tenantType),
-                                pw.Text(estab.utilities),
-                                pw.SizedBox(height: 10),
-                                pw.Text("Owner Information",
-                                    style: pw.Theme.of(context)
-                                        .defaultTextStyle
-                                        .copyWith(
-                                            fontWeight: pw.FontWeight.bold)),
-                                pw.Text(estab.ownerContact),
-                                pw.Text(estab.ownerEmail),
-                              ],
-                            ),
-                          ),
-                          pw.SizedBox(width: 10),
-                          pw.Expanded(
-                              child: pw.Column(
+    pdf.addPage(pw.MultiPage(
+      pageFormat: format,
+      build: (pw.Context context) => [
+        pw.Column(
+          children: [
+            for (var estab in estabData)
+              pw.Container(
+                  decoration: pw.BoxDecoration(border: pw.Border.all()),
+                  // padding: const pw.EdgeInsets.all(16.0),
+                  child: pw.Column(children: [
+                    pw.SizedBox(
+                      height: 10,
+                    ),
+                    pw.Row(
+                      children: [
+                        pw.SizedBox(width: 10),
+                        // IMAGE INSERT HERE
+                        pw.Expanded(
+                          child: pw.Column(
                             crossAxisAlignment: pw.CrossAxisAlignment.start,
                             mainAxisAlignment: pw.MainAxisAlignment.start,
                             children: [
-                              pw.Text("Rooms in this Establishment",
+                              pw.Text(estab.estabName,
                                   style: pw.Theme.of(context)
                                       .defaultTextStyle
                                       .copyWith(
                                           fontWeight: pw.FontWeight.bold)),
-                              for (var r in estab.rooms)
-                                pw.Row(
-                                  children: [
-                                    // pw.Icon(Icons.check_circle_outline, ) // TO IMPLEMENT: ICONS CHECK or X for availability
-                                    pw.Text(
-                                        "PHP${r.price.toString()} for ${r.capacity.toString()}"),
-                                    if (r.available)
-                                      pw.Text(" [AVAILABLE]")
-                                    else
-                                      pw.Text(" [UNAVAILABLE]")
-                                  ],
-                                )
+                              pw.Text(estab.approxLoc),
+                              // pw.Text(estab.exactLoc),
+                              pw.Text(estab.estabType),
+                              pw.Text(estab.tenantType),
+                              pw.Text(estab.utilities),
+                              pw.SizedBox(height: 10),
+                              pw.Text("Owner Information",
+                                  style: pw.Theme.of(context)
+                                      .defaultTextStyle
+                                      .copyWith(
+                                          fontWeight: pw.FontWeight.bold)),
+                              pw.Text(estab.ownerContact),
+                              pw.Text(estab.ownerEmail),
                             ],
-                          )),
-                          pw.SizedBox(width: 10)
-                        ],
-                      ),
-                      pw.SizedBox(height: 20),
-                    ]))
-            ],
-          );
-        },
-      ),
-    );
+                          ),
+                        ),
+                        pw.SizedBox(width: 10),
+                        pw.Expanded(
+                            child: pw.Column(
+                          crossAxisAlignment: pw.CrossAxisAlignment.start,
+                          mainAxisAlignment: pw.MainAxisAlignment.start,
+                          children: [
+                            pw.Text("Rooms in this Establishment",
+                                style: pw.Theme.of(context)
+                                    .defaultTextStyle
+                                    .copyWith(fontWeight: pw.FontWeight.bold)),
+                            for (var r in estab.rooms)
+                              pw.Row(
+                                children: [
+                                  // pw.Icon(Icons.check_circle_outline, ) // TO IMPLEMENT: ICONS CHECK or X for availability
+                                  pw.Text(
+                                      "PHP${r.price.toString()} for ${r.capacity.toString()}"),
+                                  if (r.available)
+                                    pw.Text(" [AVAILABLE]")
+                                  else
+                                    pw.Text(" [UNAVAILABLE]")
+                                ],
+                              )
+                          ],
+                        )),
+                        pw.SizedBox(width: 10)
+                      ],
+                    ),
+                    pw.SizedBox(height: 20),
+                  ]))
+          ],
+        )
+      ],
+    )
+        // pw.Page(
+        //   pageFormat: format,
+        //   build: (pw.Context context) {
+
+        //   },
+        // ),
+        );
     return pdf.save();
   }
 
